00b0cd6cbe500cbd685e5be07dc3e3eff82703a3
Muhammad Umair <umairsahib242@gmail.com>
2025-01-14 17:56:13 +0500

    Add files via upload

---
+++ ch5.py
@@ -0,0 +1,31 @@
+# marks = {
+#     "UMAIR": '100',
+#     "ALI": '54'
+# }
+
+# # print(
+# # marks.update({"UMAIR": "45", "SHIZAM": "58"}))
+# print(marks.get("UMAIR1"))
+# print(marks["UMAIR1"])
+
+# S = {}
+# print(type(S))
+
+S = set()
+S={65,"umair",5,5,5}
+S.add(45)
+S.add(30)
+print(S)  # Output: {45, 30}
+print(len(S))
+S.remove("umair")
+print(S)
+v={5,6,9}
+k={6,8,7,10}
+print(S.union(v))
+
+
+
+
+
+
+
