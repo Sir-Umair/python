6b75420eea273d66575b612411b774cb42b32deb
Muhammad Umair <umairsahib242@gmail.com>
2025-02-02 23:21:58 +0500

    Add files via upload

---
+++ ch7.py
@@ -0,0 +1,42 @@
+# n = int(input("ENTER NUMBER: "))
+# for i in range(1, 11):
+#     print(f"{n} × {i} = {n * i}")
+# l = ["lasim", "harry"]
+
+# for n in l:
+#     if n.startswith("l"):  # "l" سے شروع ہونے والے نام پرنٹ ہوں گے
+#         print(n)
+
+# n = int(input("ENTER NUMBER: "))
+# i=0
+# while(i<11):
+#     print(i*n)
+#     i=i+1
+
+# n = int(input("Enter a number: "))
+
+# for i in range(2, n):
+#     if(n%i) == 0:
+#         print("Number is not prime")
+#         break
+# else:
+#     print("Number is prime")
+# n=int(input("enter number :"))
+# i=1
+# sum=0
+# while(i<=n):
+#     sum=sum+i
+#     i=i+1
+      
+# print(sum)
+# 5! = 1 X 2 X 3 X 4 X 5
+
+# n = int(input("Enter the number: "))
+# product = 1
+# for i in range(1, n+1):
+#     product = product * i
+
+# print(f"The factorial of {n} is {product}")
+n = int(input("Enter the number: "))
+for i in range(1,11):
+    print(n*(11-i))
