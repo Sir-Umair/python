32fcc02d070b38c6f6713dad137ab5995bb018ff
Sir-Umair <umairsahib242@gmail.com>
2025-04-14 01:59:12 +0500

    first commit

--- ch13ps.py
+++ ch13ps.py
@@ -1,67 +1,43 @@
-class Stack:
-    def __init__(self):
-        self.stack = []
+name = input("Enter name: ")
+marks = int(input("Enter marks: "))
+phone = int(input("Phone number: "))
 
-    # Push item to the top of the stack
-    def push(self, item):
-        self.stack.append(item)
-        print(f"{item} pushed to stack.")
+s = "The name of the student is {}, his marks are {} and phone number is {}".format(name, marks, phone)
 
-    # Pop item from the top of the stack
-    def pop(self):
-        if self.is_empty():
-            print("Stack is empty. Cannot pop.")
-        else:
-            removed = self.stack.pop()
-            print(f"{removed} popped from stack.")
+print(s)
 
-    # Peek at the top element
-    def peek(self):
-        if self.is_empty():
-            print("Stack is empty. Nothing to peek.")
-        else:
-            print(f"Top element is: {self.stack[-1]}")
+# problem # 2
+n = int(input("ENTER TABLE NAME: "))
+k = [str(n * i) for i in range(1, 11)]
+s = "\n".join(k)
+print(s)
 
-    # Check if the stack is empty
-    def is_empty(self):
-        return len(self.stack) == 0
+# problem 3 
+def div(n):
+    if n%5==0:
+        return True
+    else:
+        return False
+a=[4,5,6,77,88,55,45,25]
+k=list(filter(div,a))
+print(k)
+ 
+from functools import reduce
+l=[4,5,6,77,88,55,45,25]
+def gh(a,b):
+    if a>b:
+        return a
+    return b
+b=reduce(gh,l)
+print(b)
+# problem # 4
 
-    # Display all elements
-    def display(self):
-        if self.is_empty():
-            print("Stack is empty.")
-        else:
-            print("Stack (top to bottom):")
-            for item in reversed(self.stack):
-                print(item)
+from flask import Flask
 
-# ----------------------------------------
-# Using the Stack
-s = Stack()
+app = Flask(__name__)
 
-# Push elements
-s.push(5)
-s.push(10)
-s.push(15)
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
-# Display stack
-s.display()
-
-# Peek at top
-s.peek()
-
-# Pop one item
-s.pop()
-
-# Display again
-s.display()
-
-# Pop all to empty
-s.pop()
-s.pop()
-
-# Try popping from empty stack
-s.pop()
-
-# Final peek
-s.peek()
+app.run()
